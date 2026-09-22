ad9afdf1834140ad41c1dfb86abfeb55b2d0ea0b
HAMMOUMI.W <hammoumi.w@U-3ZPKK3U5TW2Q.workspaces.services.aws.creos.internal>
2026-09-22 14:43:34 +0200

    Add CI quality gate

---
+++ test/greeting.test.js
@@ -0,0 +1,9 @@
+// Use the Node.js built-in test framework.
+const test = require("node:test");
+const assert = require("node:assert/strict");
+const { createGreeting } = require("../lib/greeting");
+
+// The pipeline must stop if this expected result changes.
+test("creates the expected greeting", () => {
+  assert.equal(createGreeting("Cloud Foundry"), "Hello Cloud Foundry");
+});
