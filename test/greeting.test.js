f0d9ace232e862aae66bd85b0cd3d7d95e14b5f1
HAMMOUMI.W <hammoumi.w@U-3ZPKK3U5TW2Q.workspaces.services.aws.creos.internal>
2026-09-23 09:44:21 +0200

    Test automatic CI/CD trigger

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -3,7 +3,7 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 const { createGreeting } = require("../lib/greeting");
 
-// The pipeline must stop if this expected result changes.
+// The pipeline must stop if this expected result changes. This is just to test the webhook.
 test("creates the expected greeting", () => {
   assert.equal(createGreeting("Cloud Foundry"), "Hello Cloud Foundry");
 });
